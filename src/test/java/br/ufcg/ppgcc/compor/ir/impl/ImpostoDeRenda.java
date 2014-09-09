072cc21a379a0adb54076c632ed325cf8b25371b
romulocgd@hotmail.com <romulocgd@hotmail.com>
2014-09-08 21:50:41 -0300

    tarefa T.01.04 finalizada

--- src/test/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
+++ src/test/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
@@ -3,12 +3,18 @@ package br.ufcg.ppgcc.compor.ir.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.ufcg.ppgcc.compor.ir.ExcecaoImpostoDeRenda;
 import br.ufcg.ppgcc.compor.ir.FachadaExperimento;
 import br.ufcg.ppgcc.compor.ir.Titular;
 
 public class ImpostoDeRenda implements FachadaExperimento{
 	private List<Titular> titulares = new ArrayList<Titular>();
+	
+	
 	public void criarNovoTitular(Titular titular){
+		if(titular.getNome()==null){
+			throw new ExcecaoImpostoDeRenda("nome obrigatório");
+		}
 		titulares.add(titular);
 	}
 	public List<Titular> listarTitulares() {
