4488a5884bf4f9fef73f0cba7870716296fc1761
romulocgd@hotmail.com <romulocgd@hotmail.com>
2014-09-08 22:30:22 -0300

    tarefa T.02.01 finalizada

--- src/test/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
+++ src/test/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
@@ -1,14 +1,18 @@
 package br.ufcg.ppgcc.compor.ir.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import br.ufcg.ppgcc.compor.ir.ExcecaoImpostoDeRenda;
 import br.ufcg.ppgcc.compor.ir.FachadaExperimento;
+import br.ufcg.ppgcc.compor.ir.FontePagadora;
 import br.ufcg.ppgcc.compor.ir.Titular;
 
 public class ImpostoDeRenda implements FachadaExperimento{
 	private List<Titular> titulares = new ArrayList<Titular>();
+	Map<Titular,List<FontePagadora>> fp = new HashMap<Titular,List<FontePagadora>>();
 	
 	
 	public void criarNovoTitular(Titular titular){
@@ -25,8 +29,18 @@ public class ImpostoDeRenda implements FachadaExperimento{
 		}
 		
 		titulares.add(titular);
+		fp.put(titular, new ArrayList<FontePagadora>());
 	}
 	public List<Titular> listarTitulares() {
 		return titulares;
+	}
+	
+	
+	public void criarFontePagadora(Titular titular, FontePagadora fonte) {					
+		fp.get(titular).add(fonte);
+	}
+	
+	public List<FontePagadora> listarFontes(Titular titular) {		
+		return fp.get(titular);
 	}
 }
